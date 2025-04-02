dd5c07dbb12317d18de22610cc3098731dac018c
soltxshio <syou_syan-01@yahoo.co.jp>
2025-04-03 01:12:44 +0900

    yuyu

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // リポジトリ名を指定（例：'/my-nextjs-app'）
+  basePath: '/yuriyuri',
+  // 静的アセットのパス設定
+  assetPrefix: '/yuriyuri/',
 };
 
 export default nextConfig;
